be00a86f8906ee0d579342d646f5086d39aa19c4
unknown <hado.roman.ihorovych@gmail.com>
2021-10-24 13:07:51 +0300

    added some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
             "css-loader",
             "sass-loader",
         ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
       }
     ]
   },
